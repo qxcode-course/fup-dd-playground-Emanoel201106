ed7248f742d355f624757b1548ff7da50db47587
Emanoel <128494693+Emanoel201106@users.noreply.github.com>
2026-04-07 00:08:23 +0000

    atividade feita

--- fup/round/src/go/draft.go
+++ fup/round/src/go/draft.go
@@ -1,5 +1,38 @@
 package main
 import "fmt"
+import "math"
+
+func ceil(num float64) int{
+    resto := math.Mod(num, 1)
+    return int((num - resto) + 1) 
+}
+
+func floor(num float64) int{
+    resto := math.Mod(num, 1)
+    return int((num - resto))
+}
+
+func round(num float64) int{
+    resto := math.Mod(num, 1)
+    if resto >= 0.5 {
+        return ceil(num)
+    } else {
+        return floor(num)
+    }
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    var operação string
+    var num float64
+   
+    fmt.Scan(&operação, &num)
+    var resultado int
+    if operação == "c" {
+        resultado = ceil(num)
+    } else if operação == "f" {
+        resultado = floor(num)
+    } else if operação == "r" {
+        resultado = round(num)
+    }
+    fmt.Println(resultado)
 }
