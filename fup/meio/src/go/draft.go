17321ea83e1504185ac3238c1771e621e483fdc2
emanoel201106 <emanoelcosmemiguel123@gmail.com>
2026-04-07 15:30:36 -0300

    atividade feita

--- fup/meio/src/go/draft.go
+++ fup/meio/src/go/draft.go
@@ -1,5 +1,13 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var esq1, esq2, esq3 int
+    fmt.Scan(&esq1, &esq2, &esq3)
+    if (esq1 > esq2 && esq1 < esq3) || (esq1 < esq2 && esq1 > esq3){
+        fmt.Println(esq1)
+    } else if (esq2 > esq1 && esq2 < esq3) || (esq2 < esq1 && esq2 > esq3){
+        fmt.Println(esq2)
+    } else {
+        fmt.Println(esq3)
+    }
 }
