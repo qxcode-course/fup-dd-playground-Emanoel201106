951694cbe3a6f05ffa8c5f4cc75120ae84190197
Emanoel <128494693+Emanoel201106@users.noreply.github.com>
2026-04-07 17:24:09 +0000

    atividade feita

--- fup/mumia/src/go/draft.go
+++ fup/mumia/src/go/draft.go
@@ -1,5 +1,18 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var nome string
+    var idade int
+    fmt.Scan(&nome, &idade)
+    if idade < 12 {
+        fmt.Println(nome, "eh crianca")
+    } else if idade < 18 {
+        fmt.Println(nome, "eh jovem")
+    } else if idade < 65 {
+        fmt.Println(nome, "eh adulto")
+    } else if idade < 1000 {
+        fmt.Println(nome, "eh idoso")
+    } else {
+        fmt.Println(nome, "eh mumia")
+    }
 }
