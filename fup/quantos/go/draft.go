02a771dd11ea10fe804da015ee2f7ad05000d7c6
Emanoel <128494693+Emanoel201106@users.noreply.github.com>
2026-04-09 16:25:17 +0000

    atividade feita

--- fup/quantos/go/draft.go
+++ fup/quantos/go/draft.go
@@ -7,7 +7,7 @@ func main() {
     	fmt.Scan(&num1, &num2, &num3)
         if num1 == num2 && num2 == num3 && num1 == num3{
     		fmt.Println("3")
-        } else if num1 == num2||num2 == num3 || num1 == num3{
+        } else if num1 == num2 || num2 == num3 || num1 == num3{
             fmt.Println("2")
         } else {
             fmt.Println("0")
