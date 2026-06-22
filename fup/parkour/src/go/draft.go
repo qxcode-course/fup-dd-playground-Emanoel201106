9ac0385244637e821b70b9be38d6470aaa4e9ede
Emanoel <128494693+Emanoel201106@users.noreply.github.com>
2026-06-22 17:43:21 +0000

    algumas atividades feitas

--- fup/parkour/src/go/draft.go
+++ fup/parkour/src/go/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var m int
+    fmt.Scan(&m)
+
+    anterior := 0
+    fmt.Scan(&anterior)
+    var x int
+    parkour := 0
+    for i := 1; i < m; i++ {
+        fmt.Scan(&x)
+        if x - anterior > 1 || x - anterior < -1 {
+            anterior = x
+            parkour++
+        } else {
+            anterior = x
+            continue
+        }
+    }
+    fmt.Println(parkour)
 }
